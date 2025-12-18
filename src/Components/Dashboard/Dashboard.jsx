ffc5029beac0e1f98454ba9e2ebfa43a83fd043b
humayonitbd <humayonforid44@gmail.com>
2025-12-18 15:23:47 +0600

    income section update

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -124,11 +124,19 @@ const Dashboard = () => {
                   >
                     <Select
                       onChange={(value) => setSelectedYear(value)}
-                      defaultValue="2024"
+                      defaultValue="2026"
                       options={[
-                        { value: "2024", label: "2024" },
-                        { value: "2023", label: "2023" },
-                        { value: "2022", label: "2022" },
+                        { value: "2025", label: "2025" },
+                        { value: "2026", label: "2026" },
+                        { value: "2027", label: "2027" },
+                        { value: "2028", label: "2028" },
+                        { value: "2029", label: "2029" },
+                        { value: "2030", label: "2030" },
+                        { value: "2031", label: "2031" },
+                        { value: "2032", label: "2032" },
+                        { value: "2033", label: "2033" },
+                        { value: "2034", label: "2034" },
+                        { value: "2035", label: "2035" },
                       ]}
                     />
                   </ConfigProvider>
